d34a435f50ff1012318f8bc819eae386f271ca32
Zhixin <zhixinchen@link.cuhk.edu.cn>
2022-09-29 12:19:56 +0800

    Initial commit

---
+++ Assignment1/source/program1/program1.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+
+int main(int argc, char *argv[]){
+
+	/* fork a child process */
+	pid_t pid;
+	int status;
+	pid = fork();
+	/* execute test program */ 
+	
+	
+	/* wait for child process terminates */
+	
+	/* check child process'  termination status */
+	
+	
+}
