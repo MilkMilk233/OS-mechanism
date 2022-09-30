fa88c618cbf950e1d6eaf8537009f3159e8beb96
Zhixin <zhixinchen@link.cuhk.edu.cn>
2022-09-30 13:52:34 +0800

    some templetes

--- Assignment1/source/program1/program1.c
+++ Assignment1/source/program1/program1.c
@@ -8,11 +8,38 @@
 int main(int argc, char *argv[]){
 
 	/* fork a child process */
-	pid_t pid;
 	int status;
-	pid = fork();
+	printf("Process start to folk\n");
+	pid_t pid = fork();
 	/* execute test program */ 
-	
+	if(pid < 0){
+		printf("Fork error!\n");
+	}
+	else{
+		//Child process
+		if(pid != 0){
+			printf("I'm the Parent Process, my pid = %d\n",getpid());
+			wait(&status);
+			printf("Parent process receives xxx signal\n");
+			exit(0);
+		}
+		else{
+			int i;
+			char *arg[argc];
+			printf("I'm the Child Process, my pid = %d\n",getpid());
+			for(i = 0; i < argc-1; i++){
+				arg[i] = argv[i+1];
+			}
+			arg[argc-1] = NULL;
+			printf("Child process start to execute test program:");
+			execve(arg[0],arg,NULL);
+
+			printf("Continue to run original child process!\n");
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return 0;
 	
 	/* wait for child process terminates */
 	
