66e9efc25bb2ee82edd1c26e93fa6ec2f40eb603
Zhixin Orange <zhixinchen@link.cuhk.edu.cn>
2022-10-10 04:18:35 +0000

    clang-format = file

--- Assignment1/source/program2/program2.c
+++ Assignment1/source/program2/program2.c
@@ -1,14 +1,14 @@
-#include <linux/module.h>
-#include <linux/sched.h>
-#include <linux/pid.h>
-#include <linux/kthread.h>
-#include <linux/kernel.h>
 #include <linux/err.h>
-#include <linux/slab.h>
-#include <linux/printk.h>
-#include <linux/jiffies.h>
-#include <linux/kmod.h>
 #include <linux/fs.h>
+#include <linux/jiffies.h>
+#include <linux/kernel.h>
+#include <linux/kmod.h>
+#include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/pid.h>
+#include <linux/printk.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 
@@ -16,7 +16,7 @@ MODULE_LICENSE("GPL");
 
 struct wait_opts {
 	enum pid_type wo_type;
-	int	wo_flags;
+	int wo_flags;
 	struct pid *wo_pid;
 
 	struct waitid_info *wo_info;
@@ -27,51 +27,51 @@ struct wait_opts {
 	int notask_error;
 };
 
-char* signal_name[] = {
-	"SIGHUP",      "SIGINT",       "SIGQUIT",      "SIGILL",      "SIGTRAP",
-	"SIGABRT",     "SIGBUS",        "SIGFPE",       "SIGKILL",     NULL,
-    "SIGSEGV",         NULL,       "SIGPIPE",     "SIGALRM",    "SIGTERM"
-};
+char *signal_name[] = { "SIGHUP",  "SIGINT", "SIGQUIT", "SIGILL",  "SIGTRAP",
+			"SIGABRT", "SIGBUS", "SIGFPE",	"SIGKILL", NULL,
+			"SIGSEGV", NULL,     "SIGPIPE", "SIGALRM", "SIGTERM" };
 
 static struct task_struct *task;
 
 extern long do_wait(struct wait_opts *wo);
 extern int do_execve(struct filename *filename,
-	const char __user *const __user *__argv,
-	const char __user *const __user *__envp);
+		     const char __user *const __user *__argv,
+		     const char __user *const __user *__envp);
 extern pid_t kernel_clone(struct kernel_clone_args *args);
-extern struct filename *getname_kernel(const char * filename);
+extern struct filename *getname_kernel(const char *filename);
 
-int my_exec(void){
+int my_exec(void)
+{
 	int result;
 	const char path[] = "/tmp/test";
-	struct filename * file_path = getname_kernel(path);
+	struct filename *file_path = getname_kernel(path);
 	printk("[program2] : child process\n");
 
 	result = do_execve(file_path, NULL, NULL);
-	if(!result){
+	if (!result) {
 		return 0;
 	}
 	do_exit(result);
 }
 
-int my_wait(pid_t pid){
+int my_wait(pid_t pid)
+{
 	int status = 0;
 	int a;
 	long retval;
-	
+
 	// int terminatedStatus;
 	struct wait_opts wo;
-	struct pid * wo_pid = NULL;
+	struct pid *wo_pid = NULL;
 	enum pid_type type;
 	type = PIDTYPE_PID;
 	wo_pid = find_get_pid(pid);
 
-	wo.wo_type   = type;
-	wo.wo_pid    = wo_pid;
-	wo.wo_flags  = WEXITED|WUNTRACED;
-	wo.wo_info   = NULL;
-	wo.wo_stat   = status;
+	wo.wo_type = type;
+	wo.wo_pid = wo_pid;
+	wo.wo_flags = WEXITED | WUNTRACED;
+	wo.wo_info = NULL;
+	wo.wo_stat = status;
 	wo.wo_rusage = NULL;
 
 	retval = do_wait(&wo);
@@ -82,24 +82,25 @@ int my_wait(pid_t pid){
 	return a;
 }
 
-//implement fork function
-int my_fork(void *argc){
-	//set default sigaction for current process
+// implement fork function
+int my_fork(void *argc)
+{
+	// set default sigaction for current process
 	int status, i;
 	struct k_sigaction *k_action = &current->sighand->action[0];
-	for(i=0;i<_NSIG;i++){
+	for (i = 0; i < _NSIG; i++) {
 		k_action->sa.sa_handler = SIG_DFL;
 		k_action->sa.sa_flags = 0;
 		k_action->sa.sa_restorer = NULL;
 		sigemptyset(&k_action->sa.sa_mask);
 		k_action++;
 	}
-	
+
 	struct kernel_clone_args kargs;
 	/* fork a process using kernel_clone or kernel_thread */
 	kargs.flags = SIGCHLD;
 	kargs.pidfd = NULL;
-	kargs.child_tid= NULL;
+	kargs.child_tid = NULL;
 	kargs.parent_tid = NULL;
 	kargs.exit_signal = SIGCHLD;
 	kargs.stack = (unsigned long)&my_exec;
@@ -115,52 +116,52 @@ int my_fork(void *argc){
 	pid_t pid = kernel_clone(&kargs);
 	/* execute a test program in child process */
 	printk("[program2] : The Child process has pid = %d\n", pid);
-	printk("[program2] : This is the parent process, pid = %d\n", (int)current->pid);
-	
+	printk("[program2] : This is the parent process, pid = %d\n",
+	       (int)current->pid);
+
 	/* wait until child process terminates */
 	status = my_wait(pid);
 	int status_id = status & 0x7f;
-	if(status_id > 0 && status_id < 16){
-		printk("[program2] : get %s signal\n", signal_name[status_id-1]);
+	if (status_id > 0 && status_id < 16) {
+		printk("[program2] : get %s signal\n",
+		       signal_name[status_id - 1]);
 		printk("[program2] : child process terminated\n");
 		printk("[program2] : The return signal is %d\n", status_id);
-	}
-	else if(status_id == 0){
+	} else if (status_id == 0) {
 		printk("[program2] : get SIGCHLD signal\n");
 		printk("[program2] : child process gets normal termination\n");
 		printk("[program2] : The return signal is %d\n", status_id);
-	}
-	else if(status_id == 127){
+	} else if (status_id == 127) {
 		printk("[program2] : get SIGSTOP signal\n");
 		printk("[program2] : child process terminated\n");
 		printk("[program2] : The return signal is 19\n");
-	}
-	else{
+	} else {
 		printk("[program2] : child process continued\n");
 	}
 	do_exit(0);
-	
+
 	return 0;
 }
 
-static int __init program2_init(void){
-
+static int __init program2_init(void)
+{
 	printk("[program2] : module_init Chen Zhixin 120090222\n");
-	
+
 	/* write your code here */
 	printk("[program2] : module_init create kthread start");
 	/* create a kernel thread to run my_fork */
 
 	task = kthread_create(&my_fork, NULL, "MyThread");
-	if(!IS_ERR(task)){
+	if (!IS_ERR(task)) {
 		printk("[program2] : module_init kthread start");
 		wake_up_process(task);
 	}
-	
+
 	return 0;
 }
 
-static void __exit program2_exit(void){
+static void __exit program2_exit(void)
+{
 	printk("[program2] : Module_exit\n");
 }
 
